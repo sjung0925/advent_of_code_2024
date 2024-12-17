740726859463fc5eff9c7b8e1aeb752db77bfe69
jjang-cute <sjung0925@github.com>
2024-12-17 16:24:13 +0900

    day1 part2

--- src/day01.rs
+++ src/day01.rs
@@ -13,9 +13,17 @@ pub fn solve() {
         })
         .unzip();
 
+    let p1_ans = part1(&col1, &col2);
+    println!("### part 1 answer is {:?}", p1_ans);
+
+    let p2_ans = part2(&col1, &col2);
+    println!("### part 2 answer is {:?}", p2_ans);
+}
+
+fn part1(c1: &Vec<i32>, c2: &Vec<i32>) -> i32 {
     // 정렬
-    let mut sorted_col1 = col1.clone();
-    let mut sorted_col2 = col2.clone();
+    let mut sorted_col1 = c1.clone();
+    let mut sorted_col2 = c2.clone();
 
     sorted_col1.sort();
     sorted_col2.sort();
@@ -29,5 +37,24 @@ pub fn solve() {
 
     // 합계
     let res: i32 = diff.iter().sum();
-    println!("result: {:?}", res);
+
+    res
+}
+
+fn part2(c1: &Vec<i32>, c2: &Vec<i32>) -> i32 {
+    let mut total_sum = 0;
+    // 컬럼 1을 순회
+    for n in c1.iter() {
+        // 현재 컬럼 1의 값과 동일 한 값이 컬럼 2에 몇개 있는지 확인
+        let mut count = 0;
+        for m in c2.iter() {
+            if n == m {
+                count += 1
+            }
+        }
+        // 현재 컬럼 1값 * 컬럼 2 속 개수
+        // 토탈 합계
+        total_sum += n * count;
+    }
+    total_sum
 }
