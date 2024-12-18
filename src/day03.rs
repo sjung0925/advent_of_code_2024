1440d34b6c13f7ec531ffb7f4b3933c5a8a7b184
jjang-cute <sjung0925@github.com>
2024-12-19 01:01:20 +0900

    day3 part2

--- src/day03.rs
+++ src/day03.rs
@@ -15,6 +15,29 @@ pub fn solve() {
 
         total_sum += x * y;
     }
+    println!("Part 1 answer is {}", total_sum);
 
-    println!("my answer is {}", total_sum);
+    let do_or_dont_re = Regex::new(r"(mul\((\d+),(\d+)\))|(do\(\))|(don't\(\))").unwrap();
+    let mut seleted_sum = 0;
+    let mut enable = true;
+
+    for t in do_or_dont_re.captures_iter(&raw_data) {
+        if let Some(_mul) = t.get(1) {
+            if enable {
+                let x2: i32 = t[2].parse().unwrap();
+                let y2: i32 = t[3].parse().unwrap();
+
+                seleted_sum += x2 * y2;
+            }
+        }
+
+        if let Some(_do_mul) = t.get(4) {
+            enable = true;
+        }
+
+        if let Some(_do_not_mul) = t.get(5) {
+            enable = false;
+        }
+    }
+    println!("Part 2 answer is {}", seleted_sum);
 }
